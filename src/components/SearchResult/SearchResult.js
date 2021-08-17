c643da7bea499fb193f75a17297022f21b64ebe5
andyjzhong <andyjzhong@gmail.com>
2021-08-17 10:29:47 -0400

    create SearchResult component

---
+++ src/components/SearchResult/SearchResult.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchResult = () => {
+    return (
+        <div className="home-screen">
+            I'm a Search Result!
+        </div>
+    )
+}
+
+export default SearchResult;
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import SearchResult from '../../components/SearchResult/SearchResult';
+
+const url = "https://audubon-api.herokuapp.com/api/birds";
 
 const SearchResults = () => {
+
+    const [performances, setPerformances] = useState([]);
+
+    useEffect(() => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((json) => {
+                console.warn("API success!", json)
+                setPerformances(json);
+            })
+            .catch(
+                console.warn("Something went wrong with the API request", console.error)
+            )
+    }, []);
+
     return (
         <div className="search-results-screen">
-            Search Results
+            <SearchResult />
         </div>
     )
 }

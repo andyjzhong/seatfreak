c41e84be62ef938fb28b26f4ee6990386200871e
andyjzhong <andyjzhong@gmail.com>
2021-08-17 09:51:05 -0400

    create SearchResults and route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './components/Home/Home';
+import SearchResults from './components/SearchResults/SearchResults';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
         <h1>Hello World</h1>
         <Route exact path="/" component={Home} />
+        <Route exact path="/search" component={SearchResults} />
     </div>
   );
 }
---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchResults = () => {
+    return (
+        <div className="search-results-screen">
+            Search Results
+        </div>
+    )
+}
+
+export default SearchResults;

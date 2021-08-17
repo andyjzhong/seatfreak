8cbb5445cefb7f727ed5f7caef03f896292c955a
andyjzhong <andyjzhong@gmail.com>
2021-08-17 09:41:52 -0400

    utilize router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import './App.css';
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className="App">
         <h1>Hello World</h1>
-        <Home />
+        <Route exact path="/" component={Home} />
     </div>
   );
 }

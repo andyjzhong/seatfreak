1fd5e8668b279a0e9171ade7aca1f16504a935ac
andyjzhong <andyjzhong@gmail.com>
2021-08-17 09:48:13 -0400

    add search input to Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="home-screen">
+            Welcome to SeatFreak!
+            <form className="search-form">
+                <input className="search-bar" type="text" placeholder="Search"/>
+            </form>
         </div>
     )
 }

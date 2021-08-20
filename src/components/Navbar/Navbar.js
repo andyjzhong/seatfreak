a810de94b6fa352e8cc20f1c1400cb1c015a1f65
andyjzhong <andyjzhong@gmail.com>
2021-08-20 18:25:18 -0400

    add favicon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { GiFocusedLightning } from 'react-icons/gi';
 import './Navbar.css';
 
 const Navbar = (props) => {
@@ -7,7 +8,7 @@ const Navbar = (props) => {
     return (
         <div className="navbar">
             <Link to={`/`}>
-                <h1 className="navbar-logo">SeatFreak</h1>
+                <h1 className="navbar-logo"><GiFocusedLightning style={{fontSize: "1.5rem"}}/> SeatFreak</h1>
             </Link>
         </div>
     )

a546a81e14c8368fa1a2ee5f30e0b213a120403d
andyjzhong <andyjzhong@gmail.com>
2021-08-22 12:25:07 -0400

    fix missing picture issue

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -8,33 +8,68 @@ const SearchResult = (props) => {
     const { currentPic, setCurrentPic } = useContext(DataContext);
 
     const handleHover = (e) => {
-        (props.dataObject._embedded.venues[0].images[0].url) ? setCurrentPic(props.dataObject._embedded.venues[0].images[0].url) : console.log("No pic");
+        setCurrentPic(props.dataObject._embedded.venues[0].images[0].url);
+
     }
 
-    return (
-        <tr className="search-result" onMouseOver={handleHover}>
-            <td>
-                <img alt="" src={props.dataObject.images[0].url} style={{width: "60px"}}/>
-            </td>
-            <td>
-                <Link to={`/search/${props.dataObject.id}`} key={props.dataObject.id}>
-                    <div><strong>{moment(props.dataObject.dates.start.localDate).format("MMM Do")}</strong></div>
-                    <div>{moment(props.dataObject.dates.start.localDate).format("ddd")} {moment(props.dataObject.dates.start.dateTime).format("h:mma")}</div>
-                </Link>
-            </td>
-            <td>
-                <Link to={`/search/${props.dataObject.id}`} key={props.dataObject.id}>
-                    <div><strong>{props.dataObject.name}</strong></div>
-                    <div>{props.dataObject._embedded.venues[0].name}</div>
-                </Link>
-            </td>
-            {props.dataObject.priceRanges ? <td>
-                <Link to={`/search/${props.dataObject.id}`} key={props.dataObject.id}>
-                    <button className="ui secondary button" style={{width: "8rem"}}>From ${Math.ceil(props.dataObject.priceRanges[0].min)}</button>
-                </Link>
-            </td> : <td><button className="ui disabled secondary button" style={{width: "8rem"}}>Unavailable</button></td>}
-        </tr>
-    )
+    const showNoPicture = () => {
+        console.log("No picture.");
+        setCurrentPic("https://i.imgur.com/jzBpkyi.jpg");
+    }
+
+    if (props.dataObject._embedded.venues[0].images) {
+        return (
+            <tr className="search-result" onMouseOver={handleHover}>
+                <td>
+                    <img alt="" src={props.dataObject.images[0].url} style={{width: "60px"}}/>
+                </td>
+                <td>
+                    <Link to={`/search/${props.dataObject.id}`} key={props.dataObject.id}>
+                        <div><strong>{moment(props.dataObject.dates.start.localDate).format("MMM Do")}</strong></div>
+                        <div>{moment(props.dataObject.dates.start.localDate).format("ddd")} {moment(props.dataObject.dates.start.dateTime).format("h:mma")}</div>
+                    </Link>
+                </td>
+                <td>
+                    <Link to={`/search/${props.dataObject.id}`} key={props.dataObject.id}>
+                        <div><strong>{props.dataObject.name}</strong></div>
+                        <div>{props.dataObject._embedded.venues[0].name}</div>
+                    </Link>
+                </td>
+                {props.dataObject.priceRanges ? <td>
+                    <Link to={`/search/${props.dataObject.id}`} key={props.dataObject.id}>
+                        <button className="ui secondary button" style={{width: "8rem"}}>From ${Math.ceil(props.dataObject.priceRanges[0].min)}</button>
+                    </Link>
+                </td> : <td><button className="ui disabled secondary button" style={{width: "8rem"}}>Unavailable</button></td>}
+            </tr>
+        )
+    } else {
+        return (
+            <tr className="search-result" onMouseOver={showNoPicture}>
+                <td>
+                    <img alt="" src={props.dataObject.images[0].url} style={{width: "60px"}}/>
+                </td>
+                <td>
+                    <Link to={`/search/${props.dataObject.id}`} key={props.dataObject.id}>
+                        <div><strong>{moment(props.dataObject.dates.start.localDate).format("MMM Do")}</strong></div>
+                        <div>{moment(props.dataObject.dates.start.localDate).format("ddd")} {moment(props.dataObject.dates.start.dateTime).format("h:mma")}</div>
+                    </Link>
+                </td>
+                <td>
+                    <Link to={`/search/${props.dataObject.id}`} key={props.dataObject.id}>
+                        <div><strong>{props.dataObject.name}</strong></div>
+                        <div>{props.dataObject._embedded.venues[0].name}</div>
+                    </Link>
+                </td>
+                {props.dataObject.priceRanges ? <td>
+                    <Link to={`/search/${props.dataObject.id}`} key={props.dataObject.id}>
+                        <button className="ui secondary button" style={{width: "8rem"}}>From ${Math.ceil(props.dataObject.priceRanges[0].min)}</button>
+                    </Link>
+                </td> : <td><button className="ui disabled secondary button" style={{width: "8rem"}}>Unavailable</button></td>}
+            </tr>
+        )
+    }
+
+
 }
 
 export default SearchResult;

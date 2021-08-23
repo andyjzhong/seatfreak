43ecb383fd1b91c501dd6f4ee5ecb9f5d167aeb4
andyjzhong <andyjzhong@gmail.com>
2021-08-23 08:24:34 -0400

    add seating sections

--- src/components/TicketTable/TicketTable.js
+++ src/components/TicketTable/TicketTable.js
@@ -13,17 +13,6 @@ const TicketTable = ({ eventDetail, showModal, setDisplayModal }) => {
             </thead>
             <tbody>
                 <tr>
-                    <td>Standing Room Only (SRO)</td>
-                    <td>SRO</td>
-                    <td>1-8 tickets</td>
-                    <td>
-                        <button className="ui orange image label deal-score">
-                            Average
-                        </button>
-                    </td>
-                    <td><button className="ui secondary button" style={{width: "8rem"}} onClick={showModal}>$84/ea</button></td>
-                </tr>
-                <tr>
                     <td>General Admission (GA)</td>
                     <td>GA</td>
                     <td>1-8 tickets</td>
@@ -35,7 +24,29 @@ const TicketTable = ({ eventDetail, showModal, setDisplayModal }) => {
                     <td><button className="ui secondary button" style={{width: "8rem"}}>$84/ea</button></td>
                 </tr>
                 <tr>
-                    <td>Premier Section</td>
+                    <td>Premier Seating</td>
+                    <td>1</td>
+                    <td>1-8 tickets</td>
+                    <td className="deal-score-col">
+                        <button className="ui blue image label deal-score">
+                            Good
+                        </button>
+                    </td>
+                    <td><button className="ui secondary button" style={{width: "8rem"}}>$84/ea</button></td>
+                </tr>
+                <tr>
+                    <td>Gold Premier Seating</td>
+                    <td>1</td>
+                    <td>1-8 tickets</td>
+                    <td className="deal-score-col">
+                        <button className="ui blue image label deal-score">
+                            Good
+                        </button>
+                    </td>
+                    <td><button className="ui secondary button" style={{width: "8rem"}}>$84/ea</button></td>
+                </tr>
+                <tr>
+                    <td>Platinum Premier Seating</td>
                     <td>1</td>
                     <td>1-8 tickets</td>
                     <td className="deal-score-col">
@@ -66,6 +77,17 @@ const TicketTable = ({ eventDetail, showModal, setDisplayModal }) => {
                         </button>
                     </td>
                     <td><button className="ui secondary button" style={{width: "8rem"}}>$84/ea</button></td>
+                </tr>
+                <tr>
+                    <td>Standing Room Only (SRO)</td>
+                    <td>SRO</td>
+                    <td>1-8 tickets</td>
+                    <td>
+                        <button className="ui orange image label deal-score">
+                            Average
+                        </button>
+                    </td>
+                    <td><button className="ui secondary button" style={{width: "8rem"}} onClick={showModal}>$84/ea</button></td>
                 </tr>
             </tbody>
         </table>

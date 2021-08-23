05ac395f1fcf392990efd56fffb4126e1a866a3a
andyjzhong <andyjzhong@gmail.com>
2021-08-23 14:56:24 -0400

    style navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,10 +6,14 @@ import './Navbar.css';
 
 const Navbar = (props) => {
 
+    if (window.location.href.indexOf("search") != -1) {
+        console.log("POW");
+    }
+
     const {width} = useContext(DataContext);
 
     return (
-        <div className="navbar" style={(width <= 800) ? {backgroundColor: "white"} : {backgroundColor: "rgba(255, 255, 255, 0)"}}>
+        <div className="navbar" style={(width <= 800) ? {backgroundColor: "white"} : (window.location.href.indexOf("search") !== -1) ? {backgroundColor: "white"} : {backgroundColor: "rgba(255, 255, 255, 0)"}}>
             <Link to={`/`}>
                 <h1 className="navbar-logo" style={(width <= 800) ? {textAlign: "center"} : {textAlign: "left"}}><GiFocusedLightning style={{fontSize: "1.5rem"}}/> SeatFreak</h1>
             </Link>
